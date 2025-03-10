ce3a686920bf3397a39c62040229ae847f72444c
Ramesh <officialrameshkrr652@gmail.com>
2025-03-10 20:23:25 +0530

    data extract initial

--- src/constants/platforms.js
+++ src/constants/platforms.js
@@ -1,18 +1,19 @@
 // src/constants/platforms.js
 export default [
-    {
-      id: 'swiggy',
-      name: 'Swiggy',
-      color: '#FC8019',
-      icon: 'fastfood',
-      emailQuery: 'from:noreply@swiggy.in',
-    },
+    
     {
       id: 'zomato',
       name: 'Zomato',
       color: '#CB202D',
       icon: 'restaurant',
       emailQuery: 'from:noreply@zomato.com',
+    },
+    {
+      id: 'swiggy',
+      name: 'Swiggy',
+      color: '#FC8019',
+      icon: 'fastfood',
+      emailQuery: 'from:noreply@swiggy.in',
     },
     {
       id: 'flipkart',
